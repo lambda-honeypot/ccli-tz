1479e89b213e3df1c8dbc99cb331222fdf641d11
Chris Berry <bez625@gmail.com>
2022-09-21 16:23:40 +0100

    Add error if cant find file

--- cmd/root.go
+++ cmd/root.go
@@ -101,5 +101,7 @@ func initConfig() {
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	} else {
+		log.Fatalf("failed to read config file at %s", config.FileName)
 	}
 }

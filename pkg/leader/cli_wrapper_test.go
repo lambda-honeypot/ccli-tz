3a4f565500571e9a8eb4d09f23347e8b6d87077b
Chris Berry <bez625@gmail.com>
2022-09-27 15:34:24 +0100

    Fix issue with mainnet (#6)

---
+++ pkg/leader/cli_wrapper_test.go
@@ -0,0 +1,20 @@
+package leader
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("CalculateArgs", func() {
+	It("should only return non plank args", func() {
+		period := "--current"
+		shelleyGenesisFile := "shelley-genesis.json"
+		poolID := "5be57ce6d1225697f4ad4090355f0a72d6e1e2446d1d768f36aa118c"
+		vrfKeysFile := "vrf.skey"
+		testnetMagic := ""
+		resultArgs := CalculateArgs(period, shelleyGenesisFile, poolID, vrfKeysFile, testnetMagic)
+		for _, arg := range resultArgs {
+			Expect(arg).ToNot(Equal(""))
+		}
+	})
+})

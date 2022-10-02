bfd493837de2c2f0ce1deba897ea16a4ae297562
Chris Berry <chris.berry@sky.uk>
2022-10-02 18:52:56 +0100

    Minor refactor and add epoch tip info

--- pkg/leader/cli_wrapper_test.go
+++ pkg/leader/cli_wrapper_test.go
@@ -5,14 +5,14 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("CalculateArgs", func() {
+var _ = Describe("CalculateLeaderArgs", func() {
 	It("should only return non plank args", func() {
 		period := "--current"
 		shelleyGenesisFile := "shelley-genesis.json"
 		poolID := "5be57ce6d1225697f4ad4090355f0a72d6e1e2446d1d768f36aa118c"
 		vrfKeysFile := "vrf.skey"
 		testnetMagic := ""
-		resultArgs := CalculateArgs(period, shelleyGenesisFile, poolID, vrfKeysFile, testnetMagic)
+		resultArgs := CalculateLeaderArgs(period, shelleyGenesisFile, poolID, vrfKeysFile, testnetMagic)
 		for _, arg := range resultArgs {
 			Expect(arg).ToNot(Equal(""))
 		}
